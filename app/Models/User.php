decc0eefa15a157c8d6525fbc4be33bf7634b08d
HaseebHassan <mhhassan095@gmail.com>
2021-12-14 16:38:21 -0800

    second ommit

---
+++ app/Models/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use Exception;
+use Twilio\Rest\Client;
+use App\Models\UserCode;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function generateCode()
+    {
+        $code = rand(1000, 9999);
+  
+        UserCode::updateOrCreate(
+            [ 'user_id' => auth()->user()->id ],
+            [ 'code' => $code ]
+        );
+  
+        $receiverNumber = auth()->user()->phone;
+        $message = "2FA login code is ". $code;
+    
+        try {
+   
+            $account_sid = getenv("TWILIO_SID");
+            $auth_token = getenv("TWILIO_TOKEN");
+            $twilio_number = getenv("TWILIO_FROM");
+    
+            $client = new Client($account_sid, $auth_token);
+            $client->messages->create("+923122350429", [
+                'from' => $twilio_number, 
+                'body' => $message]);
+    
+        } catch (Exception $e) {
+            info("Error: ". $e->getMessage());
+        }
+    }
+}
